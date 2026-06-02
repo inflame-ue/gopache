ed1195c1b5652fbe4ec31c449a5d8de948ce934f
Ilia Oberemok <i.oberemok777@gmail.com>
2026-06-02 20:33:22 +0300

    add interrupt handling so that the cache could be saved on exit

--- cmd/proxy/main.go
+++ cmd/proxy/main.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"os/signal"
 
 	"github.com/inflame-ue/gopache/internal/cache"
 	"github.com/inflame-ue/gopache/internal/config"
@@ -23,10 +25,25 @@ func main() {
 	if proxyConfig.FlushCache {
 		cacheMap.Flush()
 		log.Print("cache flushed succesfully")
+		os.Exit(0)
 	}
 
 	proxy := proxy.NewProxy(client, cacheMap, proxyConfig.Origin)
 	addr := fmt.Sprintf(":%d", proxyConfig.Port)
+
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		<-c
+		log.Print("interrupt received...serializing cache and exiting...")
+		cacheMap.Save("test.json")
+		os.Exit(0)
+	}()
+	
 	log.Printf("listening on port: %d", proxyConfig.Port)
-	http.ListenAndServe(addr, proxy)
+	err = http.ListenAndServe(addr, proxy)
+	if err != nil {
+		log.Print("something went horribly wrong...")
+		os.Exit(1)
+	}
 }
